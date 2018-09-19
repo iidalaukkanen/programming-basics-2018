becdf9cb18e2de6845ef275b1697a88487537334
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-09-19 09:19:05 +0300

    Condition folder defined

---
+++ condition-tasks/IfTask1/IfTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IfTask1
+{
+    // This one line comment
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Ohjelma selvittää, onko annettu luku positiivinen, negatiivinen vai nolla");
+            Console.WriteLine("Syötä luku");
+        }
+    }
+}
