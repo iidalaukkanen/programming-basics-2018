50cd5c18533b4c33c9bfc8ded3cb1851a5c1368a
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-09-30 19:03:15 +0300

    New program

---
+++ condition-tasks/IfTask4/IfTask4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IfTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma järjestää kolme lukua nousevaan järjestykseen");
+            Console.Write("Syötä 1. luku:");
+            string userInput = Console.ReadLine();
+            int x = int.Parse(userInput);
+
+            Console.Write("Syötä 2. luku:");
+            userInput = Console.ReadLine();
+            int y = int.Parse(userInput);
+
+            Console.Write("Syötä 3. luku:");
+            userInput = Console.ReadLine();
+            int z = int.Parse(userInput);
+
+            if (x < y && x < z)
+                if (y < z)
+                    Console.WriteLine($"{x},{y},{z}");
+                else
+                    Console.WriteLine($"{x},{z},{y}");
+
+            else if (y < x && y < z)
+                if (x < z)
+                    Console.WriteLine($"{y},{x},{z}");
+                else
+                    Console.WriteLine($"{y},{z},{x}");
+
+            else if (z < x && z < y)
+                if (x < y)
+                    Console.WriteLine($"{z},{x},{y}");
+                else
+                    Console.WriteLine($"{z},{y},{x}");
+
+        }
+    }
+}
