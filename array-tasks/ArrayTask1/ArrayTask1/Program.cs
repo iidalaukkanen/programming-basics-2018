3af5e5b6347f847e8363d64c05488d407db9b8d1
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-10-31 13:47:28 +0200

    ArrayTask1 prigram done

---
+++ array-tasks/ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            decimal[] productPrice = new decimal[] { 7.96m, 72.0m, 99.90m };
+            int[] productAmount = new int[] { 1, 3, 2 };
+            decimal[] productTotalPrice = new decimal[3];
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote{i + 1}, {productTotalPrice[i]:c}");
+
+            }
+        }
+    }
+}
