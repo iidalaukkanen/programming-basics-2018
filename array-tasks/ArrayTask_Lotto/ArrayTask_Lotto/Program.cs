a1d0f12ea8a19fddea6c98906c431f65ef29921c
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-10-31 15:56:08 +0200

    ArrayTask_Lotto done

---
+++ array-tasks/ArrayTask_Lotto/ArrayTask_Lotto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ArrayTask_Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo lauantain lottonumerot ja tulostaa arvtotut luvut");
+            int[] lottoNumbers = new int[40];
+            Random rnd = new Random();
+  
+
+            for (int i = 0; i < 7; i++)
+            {
+                int rndNumber = rnd.Next(40);
+                if (lottoNumbers[rndNumber] == 0)
+                    lottoNumbers[rndNumber] = 1;
+                else
+                    i--;                
+            }
+            while (true)
+            {
+                int rndNumber = rnd.Next(40);
+                if (lottoNumbers[rndNumber] == 0)
+                {
+                    lottoNumbers[rndNumber] = 2;
+                    break;
+                }
+
+               
+               
+            }
+            int plusNumber = rnd.Next(40)+1;
+            Console.Write("Loton oikea rivi:");
+            for (int i = 0; i < 40; i++)
+            {
+                if (lottoNumbers[i] == 1)
+                    Console.Write($"{i + 1} ");
+            }
+            
+            for (int i = 0; i < 40; i++)
+            {
+                if (lottoNumbers[i] == 2)
+                    Console.WriteLine($"\nLisänumero: {i + 1}\t");
+            }
+            Console.WriteLine($"Plusnumero: {plusNumber}");
+        }
+    }
+}
