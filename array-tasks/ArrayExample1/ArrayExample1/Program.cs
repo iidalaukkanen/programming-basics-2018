514d4dc6cf3246769b867425f18d4c482124bb9e
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-10-31 13:07:59 +0200

    ArrayExample1 program done

---
+++ array-tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaa kokonaislukutaulukon (10 alkiota), johon arvotaan lukuja 0...100");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);         
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+            Console.WriteLine("Ohjelman suoritus on päättynyt");
+        }
+    }
+}
