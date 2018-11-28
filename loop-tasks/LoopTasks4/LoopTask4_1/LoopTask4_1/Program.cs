a3e18d83de004c75f564c8f433add0adc0ea17e0
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-11-28 11:41:42 +0200

    LoopTask4_1 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_1/LoopTask4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LoopTask4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 1-10 ja niiden neliöjuuret.");
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{i}.\t{Math.Sqrt(i)}");
+            }
+        }
+    }
+}
