7323b28119cc509fe321e2e0a0a887b608259bb6
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-11-28 08:57:25 +0200

    LoopTask3_1 v1.0

---
+++ loop-tasks/LoopTask3_1/LoopTask3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoopTask3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma selvittää syöttämäsi numeron kertoman.");
+            int number = 0;
+            int fact = 1;
+
+            do
+            {
+                Console.WriteLine("Syötä numero: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                    Console.WriteLine("Väärä syöte!");
+            }
+            while (number <= 0);
+
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine($"{number}! = {fact}");
+
+        }
+    }
+}
