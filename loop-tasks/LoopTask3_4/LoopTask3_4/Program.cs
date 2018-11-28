f55b5fd0a559c5ad893aa249e1d3d815f41c6df2
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-11-28 10:42:05 +0200

    LoopTask3_4 v1.0

---
+++ loop-tasks/LoopTask3_4/LoopTask3_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopTask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee ensimmäiset luvut yhteen.");
+            int number = 0;
+            int sum = 0;
+            
+
+            Console.WriteLine("Syötä viimeinen numero: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    sum = sum + i;
+                }
+            }
+            else
+                for (int i = 0; i >= number; i--)
+                {
+                    sum = sum + i;
+                }
+            Console.WriteLine($"Lukujen summa on: {sum}");
+     
+        }
+    }
+}
