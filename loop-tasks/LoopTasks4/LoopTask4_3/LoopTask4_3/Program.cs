c91d32b6b1537bdeb2768003a0cc1e5afbdc3b35
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-05 09:47:18 +0200

    LoopTask4.3 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_3/LoopTask4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopTask4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista lukua väliltä [0-50].");
+            int number = 1;
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Rivi {i}: ");
+                for (int j = 1; j <= 5; j++)
+                {
+                    number = rnd.Next(51);
+                    if (j == 5)
+                        Console.WriteLine($"{number}");
+                    else
+                        Console.Write($"{number}, ");
+                }
+            }
+        }
+    }
+}
