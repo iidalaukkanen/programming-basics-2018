ee984f9e42519bbf6a87323d49b4ac68dbd9d56d
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-05 09:03:03 +0200

    LoopTask4_2 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_2/LoopTask4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopTask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9.");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write($"{(i * j):d2}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
