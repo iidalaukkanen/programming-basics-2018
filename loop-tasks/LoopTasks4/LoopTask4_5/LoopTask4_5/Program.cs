90adadab7f0d08e0465ea1cd34f5b9a505ff51a6
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-10 21:33:10 +0200

    LoopTask4.5 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_5/LoopTask4_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoopTask4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vakioveikkauksen rivisi on arvottu:\n");
+            double odds;
+
+            Random rnd = new Random();
+            for (int i = 1; i <= 13; i++)
+            {
+                odds = rnd.NextDouble();
+                if (odds <= 0.4)
+                {
+                    Console.WriteLine($"{i}.\t1");
+                }
+                else if (odds <= 0.6)
+                {
+                    Console.WriteLine($"{i}.\tX");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}.\t2");
+                }
+            }
+        }
+    }
+}
