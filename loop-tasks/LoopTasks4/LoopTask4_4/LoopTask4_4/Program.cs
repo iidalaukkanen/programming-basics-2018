60804ac9a050c03db8fc64f78f692fea2e19415a
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-10 21:17:17 +0200

    LoopTask4.4 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_4/LoopTask4_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LoopTask4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa saatujen kruunien ja klaavojen määrän.");
+            int heads = 0;
+            int tails = 0;
+            int n;
+            Random rnd = new Random();
+            int rndNumber;
+
+            Console.Write("Syötä heittojen lukumäärä: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                rndNumber = rnd.Next(2);
+                if (rndNumber == 1)
+                {
+                    heads = heads + 1;
+                }
+                else
+                {
+                    tails = tails + 1;
+                }
+            }
+            Console.WriteLine($"Kruuna heitettiin {heads} kertaa ja klaava heitettiin {tails} kertaa.");
+
+        }
+    }
+}
