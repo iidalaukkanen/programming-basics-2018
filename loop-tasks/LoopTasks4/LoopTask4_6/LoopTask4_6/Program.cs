8e945f42a8e8f41d78c4e68807d21d797cdabd39
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-10 21:52:30 +0200

    LoopTask4.6 v1.0

---
+++ loop-tasks/LoopTasks4/LoopTask4_6/LoopTask4_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LoopTask4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo tulokset 1000:sta nopan heitosta.");
+            Random rnd = new Random();
+            int rndNumber;
+            int counter = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                rndNumber = rnd.Next(1,7);
+                if (rndNumber == 6)
+                {
+                    counter = counter + 1;
+                }
+            }
+            Console.WriteLine($"Kuutonen heitettiin {counter} kertaa.");
+        }
+    }
+}
