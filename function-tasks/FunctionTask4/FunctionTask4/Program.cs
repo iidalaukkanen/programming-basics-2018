a3daffe6480c104d0f6ece9298d4ec98067e11fa
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-16 23:16:39 +0200

    Self-evaluation Task 1 done

---
+++ function-tasks/FunctionTask4/FunctionTask4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FunctionTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxIndex = 0;
+            Console.WriteLine("Syötä 10 positiivista lukua: ");
+            Console.WriteLine($"Suurin luku oli {Numbers(ref maxIndex)} ja se oli {maxIndex}.");
+            Console.ReadKey();
+        }
+        static int Numbers(ref int maxIndex)
+        {
+            int nbr = 0;
+            int max = 0;
+            bool isNumber;
+            string output = "";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}.");
+                isNumber = int.TryParse(Console.ReadLine(), out nbr);
+                if (isNumber && nbr > 0)
+                {
+                    output += $"{nbr} ";
+                    if (max < nbr)
+                    {
+                        max = nbr;
+                        maxIndex = i + 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte! Syötä positiivinen kokonaisluku: ");
+                }
+            }
+            Console.WriteLine($"Syötit seuraavat luvut:\n{output}\n");
+            return max;
+        }
+    }
+}
---
+++ self-evaluation/SelfEvaluationTask1.1/SelfEvaluationTask1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SelfEvaluationTask1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa syöttämäsi lausahduksen viisi kertaa.");
+            Console.WriteLine("Kirjoita jotain: ");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(userInput);
+            }
+        }
+    }
+}
