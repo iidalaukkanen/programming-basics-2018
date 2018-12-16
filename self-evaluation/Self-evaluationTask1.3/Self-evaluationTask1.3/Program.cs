813bb23871fa8bc47b3431625da02cac0135c190
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-16 23:32:02 +0200

    Self-evaluation Task 3 done

---
+++ self-evaluation/Self-evaluationTask1.3/Self-evaluationTask1.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Self_evaluationTask1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötettyjen lukujen summan, kunnes syötät luvun -1.");
+            Console.WriteLine("Syötä lukuja: ");
+            int userInput;
+            int sum = 0;
+            do
+            {
+                while (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+
+                sum = sum + userInput;
+            }
+            while (userInput != -1);
+
+            Console.WriteLine($"Lukujen summa on {sum}.");
+        }
+    }
+}
