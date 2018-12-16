09afa087290ec0d7960d5f6dc435047256575586
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-16 17:12:04 +0200

    FunctionTask3 v1.0

---
+++ function-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FunctionTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = NumberFromRange(1,100);
+            Print();
+        }
+
+        static int NumberFromRange(int lowerBound, int upperBound)
+        {
+            bool isNumber;
+            int input;
+
+            do
+            {
+                Console.Write($"Syötä luku väliltä {lowerBound} - {upperBound}: ");
+                isNumber = int.TryParse(Console.ReadLine(), out input);
+
+                if (isNumber && input >= lowerBound && input <= upperBound)
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine($"Väärä syöte!");
+                }
+            }
+            while (true);
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Luku kelvollinen!");
+        }
+    }
+}
