b7923c7ad880a78b72890def9c684a4724cfca66
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-16 23:24:00 +0200

    Self-evaluationTask2 done

---
+++ self-evaluation/Self-evaluationTask1.2/Self-evaluationTask1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Self_evaluationTask1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kirjoittamasi lauseen yhtä monta kertaa kuin siinä on kirjaimia.");
+            Console.WriteLine("Kirjoita jotain: ");
+            string userInput = Console.ReadLine();
+
+            int k = userInput.Length;
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine($"{i}. {userInput}");
+            }
+        }
+    }
+}
