53d46006b2276f5db5df12b0fd4a342f115d6606
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-19 18:47:55 +0200

    ArrayTask3.2 done

---
+++ array-tasks/ArrayTasks3/ArrayTask3_2/ArrayTask3_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayTask3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo taulukkoon luvut väliltä 0-99.");
+
+            int[,] raffle = new int[10, 20];
+            RndRaffle(ref raffle);
+        }
+
+        static void RndRaffle(ref int[,] arrRaffle)
+        {
+            Random rnd = new Random();
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine();
+                for (int y = 0; y < 20; y++)
+                {
+                    arrRaffle[x, y] = rnd.Next(100);
+                    Console.WriteLine($"[{x},{y}] = {arrRaffle[x,y]:d2}");
+                }
+            }
+        }
+    }
+}
