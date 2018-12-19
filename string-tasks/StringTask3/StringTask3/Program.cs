8b2d7ef5a17a7dcc521492fbbeb2832fabad0202
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-19 19:38:43 +0200

    StringTask3 done

---
+++ string-tasks/StringTask3/StringTask3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötteestä o-kirjaimet.");
+            Console.WriteLine("Kirjoita jotain: ");
+            string input = Console.ReadLine();
+
+            int letterCounter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input.Substring(i,1).ToUpper()== "O")
+                {
+                    letterCounter++;
+                }
+            }
+            Console.WriteLine("Lauseessa '{0}' on {1} O-kirjainta.", input.ToUpper(), letterCounter);
+        }
+    }
+}
