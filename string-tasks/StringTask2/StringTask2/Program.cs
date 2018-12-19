1367194bac77f599013dc497a685d900f12e7ae0
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-19 19:28:04 +0200

    StringTask2 done

---
+++ string-tasks/StringTask2/StringTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa kirjoittamasi lauseen kaikki e-kirjaimet @-merkeiksi.");
+            Console.WriteLine("Kirjoita jotain: ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.Replace("e","@"));
+        }
+    }
+}
