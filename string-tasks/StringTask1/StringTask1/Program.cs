660f8fc10bbb60446144843ce2636449af5735eb
iidalaukkanen <iida.laukkanen@hotmail.com>
2018-12-19 19:24:04 +0200

    StringTask1 done

---
+++ string-tasks/StringTask1/StringTask1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötettyjen merkkien määrän.");
+            Console.WriteLine("Kirjoita jotain: ");
+            string k = Console.ReadLine();
+            Console.WriteLine($"Lauseessa '{k}' on {k.Length} merkkiä.");
+        }
+    }
+}
