452b992521610b6534cda012c96fe68f91e1eb3e
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-01-06 23:57:10 +0200

    ViitenumeronTarkistus v1.0

---
+++ ViitenumeronTarkistus/ViitenumeronTarkistus/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace ViitenumeronTarkistus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    string refNumber = RefNumber();
+                    if (LengthChecker(refNumber) == false)
+                        break;
+                    string tmpRefNumber = Reverse(refNumber.Substring(0, refNumber.Length - 1));
+                    string checkNumber = CheckNumberCreator(tmpRefNumber);
+                    refNumber = SpaceMaker(refNumber);
+                    if (Checker(refNumber.Substring(refNumber.Length - 1, 1), checkNumber) == true)
+                    {
+                        PrintTrue(refNumber);
+                    }
+                    else
+                        PrintFalse();
+                    break;
+
+                case 2:
+                    break;
+            }
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine("1. Ohjelma tarkistaa kotimaisen viitenumeron.\n2. Ohjelma luo kotimaisen viitenumeron.\n3. Ohjelma luo halutun määrän viitenumeroita.");
+        }
+
+        static string RefNumber()
+        {
+            Console.Write("Syötä viitenumero: ");
+            return Console.ReadLine();
+        }
+
+        static bool LengthChecker(string s)
+        {
+            if (s.Length < 4 || s.Length > 20)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static string CheckNumberCreator(string s)
+        {
+            string checkMark = string.Empty;
+            int sum = 0;
+            int[] list = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                list[i] = int.Parse(s.Substring(i, 1));
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    list[i] = list[i] * 7;
+                }
+                else if (i % 3 == 1)
+                {
+                    list[i] = list[i] * 3;
+                }
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+
+            int difference = (int)(Math.Ceiling(sum / 10.0d) * 10);
+            checkMark = (difference - sum).ToString();
+            return checkMark;
+        }
+
+        static string SpaceMaker(string s)
+        {
+            int space = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (space > 2)
+                    break;
+                if (i >= 5 && i % 5 == 0)
+                {
+                    s = s.Insert(s.Length - i - space, " ");
+                    space = space + 1;
+                }
+            }
+            return s;
+        }
+         
+        static bool Checker(string s1, string s2)
+        {
+            if (s1 == s2)
+                return true;
+            else
+                return false;
+        }
+
+        static void PrintTrue(string s)
+        {
+            Console.WriteLine("OK.");
+            Console.WriteLine(s);
+        }
+
+        static void PrintFalse()
+        {
+            Console.WriteLine("Viitenumerosi on väärä.");
+        }
+    }
+}
---
+++ string-tasks/testi/testi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllCheck("131052-308T", 131052308);
+        }
+
+        static void AllCheck(string id, int fullNumber)
+        {
+            string checkId = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+            int idModulo = fullNumber % 31;
+            if (id[10] != checkId[idModulo])
+                Console.WriteLine("Henkilötunnus on virheellinen.");
+            else
+                Console.WriteLine($"Henkilötunnuksen {id} oikea tarkaste on: {checkId[idModulo]}.");
+        }
+
+    }
+}
